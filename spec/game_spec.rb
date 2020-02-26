e0778713cf3f64fe2942c45c0b402d1aa7207514
AliciaMerlino <acmerlino1@gmail.com>
2020-02-26 12:35:56 +0000

    Empty board created on instantiation

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,9 @@
 require 'game'
 
 describe Game do
+
+  it 'Shows the game field to players' do
+    expect(subject.board).to eq [[" ", "|", " ", "|", " "], ["----------"], [" ", "|", " ", "|", " "], ["----------"], [" ", "|", " ", "|", " "]]
+  end
   
 end
