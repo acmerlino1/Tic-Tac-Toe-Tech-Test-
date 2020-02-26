65f83cfaac27dc87d42654f6249565a557922dcc
AliciaMerlino <acmerlino1@gmail.com>
2020-02-26 14:40:06 +0000

    Player can choose middle row and left square for move

--- lib/move.rb
+++ lib/move.rb
@@ -1,12 +1,17 @@
 class Move
 
   def player_move(row, column)
-    if column == 'left'
-      {row: 0, column: 0}
+    if column == 'left' 
+      if row == 'top'
+        {row: 0, column: 0}
+      else
+        {row: 2, column: 0}
+      end
     elsif column == 'right'
       {row: 0, column: 4}
     else
       {row: 0, column: 2}
     end
   end
+
 end
--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -20,6 +20,12 @@ describe Move do
       column = 'right'
       expect(subject.player_move(row, column)).to eq({row: 0, column: 4})
     end
+
+    it 'Returns a hash with players moves as co-ordinates 2 and 0' do
+      row = 'middle'
+      column = 'left'
+      expect(subject.player_move(row, column)).to eq({row: 2, column: 0})
+    end
   end
 
 end
