8db391fc3902950435ddd21f206f7155f2c8261c
AliciaMerlino <acmerlino1@gmail.com>
2020-02-26 14:31:37 +0000

    Player can choose right column and co-ordinates change

--- lib/move.rb
+++ lib/move.rb
@@ -3,6 +3,8 @@ class Move
   def player_move(row, column)
     if column == 'left'
       {row: 0, column: 0}
+    elsif column == 'right'
+      {row: 0, column: 4}
     else
       {row: 0, column: 2}
     end
--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -13,7 +13,13 @@ describe Move do
       row = 'top'
       column = 'middle'
       expect(subject.player_move(row, column)).to eq({row: 0, column: 2})
-    end   
+    end 
+    
+    it 'Returns a hash with players moves as co-ordinates 0 and 4' do
+      row = 'top'
+      column = 'right'
+      expect(subject.player_move(row, column)).to eq({row: 0, column: 4})
+    end
   end
 
 end
