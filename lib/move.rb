effef86d25ceac574d2805f07a0f24f3c95702c1
AliciaMerlino <acmerlino1@gmail.com>
2020-02-26 15:49:15 +0000

    refactor players move method

--- lib/move.rb
+++ lib/move.rb
@@ -10,31 +10,18 @@ class Move
     column
   end
 
-  def row(user_input = gets.chomp)
+  def row(user_row = gets.chomp)
     rows = {'top' => 0, 'middle' => 2, 'bottom' => 4}
-    rows[user_input]
+    rows[user_row]
   end
 
-  def column(user_input = gets.chomp)
+  def column(user_column = gets.chomp)
     columns = {'left' => 0, 'middle' => 2, 'right' => 4}
-    columns[user_input]
+    columns[user_column]
   end
 
   def player_move(row, column)
-    
-    if column == 'left' 
-      if row == 'top'
-        {row: 0, column: 0}
-      elsif row == 'bottom'
-        {row: 4, column: 0}
-      else
-        {row: 2, column: 0}
-      end
-    elsif column == 'right'
-      {row: 0, column: 4}
-    else
-      {row: 0, column: 2}
-    end
+    coordinates = {row: row, column: column}
   end
 
 end
--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -4,67 +4,67 @@ describe Move do
   
   describe '#player_move' do
     it 'Returns a hash with player move as co-ordinates 0 and 0' do
-      row = 'top'
-      column = 'left'
-      expect(subject.player_move(row, column)).to eq({row: 0, column: 0})
+      user_row = 'top'
+      user_column = 'left'
+      expect(subject.player_move(subject.row(user_row), subject.column(user_column))).to eq({row: 0, column: 0})
     end
     
     it 'Returns a hash with players moves as co-ordinates 0 and 2' do
-      row = 'top'
-      column = 'middle'
-      expect(subject.player_move(row, column)).to eq({row: 0, column: 2})
+      user_row = 'top'
+      user_column = 'middle'
+      expect(subject.player_move(subject.row(user_row), subject.column(user_column))).to eq({row: 0, column: 2})
     end 
     
     it 'Returns a hash with players moves as co-ordinates 0 and 4' do
-      row = 'top'
-      column = 'right'
-      expect(subject.player_move(row, column)).to eq({row: 0, column: 4})
+      user_row = 'top'
+      user_column = 'right'
+      expect(subject.player_move(subject.row(user_row), subject.column(user_column))).to eq({row: 0, column: 4})
     end
 
     it 'Returns a hash with players moves as co-ordinates 2 and 0' do
-      row = 'middle'
-      column = 'left'
-      expect(subject.player_move(row, column)).to eq({row: 2, column: 0})
+      user_row = 'middle'
+      user_column = 'left'
+      expect(subject.player_move(subject.row(user_row), subject.column(user_column))).to eq({row: 2, column: 0})
     end
 
     it 'Returns a hash with players moves as co-ordinates 4 and 0' do
-      row = 'bottom'
-      column = 'left'
-      expect(subject.player_move(row, column)).to eq({row: 4, column: 0})
+      user_row = 'bottom'
+      user_column = 'left'
+      expect(subject.player_move(subject.row(user_row), subject.column(user_column))).to eq({row: 4, column: 0})
     end
   end
 
   describe '#row' do
     it 'player enters row as top, returns 0' do
-      user_input = 'top'
-      expect(subject.row(user_input)).to eq(0)
+      user_row = 'top'
+      expect(subject.row(user_row)).to eq(0)
     end
 
     it 'player enters row as middle, returns 2' do
-      user_input = 'middle'
-      expect(subject.row(user_input)).to eq(2)
+      user_row = 'middle'
+      expect(subject.row(user_row)).to eq(2)
     end
     
     it 'player enters row as bottom, returns 4' do
-      user_input = 'bottom'
-      expect(subject.row(user_input)).to eq(4)
+      user_row = 'bottom'
+      expect(subject.row(user_row)).to eq(4)
     end
   end
 
   describe '#column' do
     it 'player enters left column, returns 0' do
-      user_input = 'left'
-      expect(subject.column(user_input)).to eq(0)
+      user_column = 'left'
+      expect(subject.column(user_column)).to eq(0)
     end
 
     it 'player enters middle column, returns 2' do
-      user_input = 'middle'
-      expect(subject.column(user_input)).to eq(2)
+      user_column = 'middle'
+      expect(subject.column(user_column)).to eq(2)
     end
 
     it 'player enters right column, returns 4' do
-      user_input = 'right'
-      expect(subject.column(user_input)).to eq(4)
+      user_column = 'right'
+      expect(subject.column(user_column)).to eq(4)
     end
   end
 
