d283ec58bc49a1fa8a8f35f84d0a66eafe76cb51
AliciaMerlino <acmerlino1@gmail.com>
2020-02-26 14:25:54 +0000

    Takes player move and passes it into a hash

--- lib/move.rb
+++ lib/move.rb
@@ -1,2 +1,6 @@
 class Move
+
+  def player_move(row, column)
+    {row: 0, column: 0}
+  end
 end
--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -2,5 +2,12 @@ require 'move'
 
 describe Move do
   
+  describe '#player_move' do
+    it 'Adds X on board when first player moves' do
+      row = 'top'
+      column = 'left'
+      expect(subject.player_move(row, column)).to eq({row: 0, column: 0})
+    end    
+  end
 
 end
