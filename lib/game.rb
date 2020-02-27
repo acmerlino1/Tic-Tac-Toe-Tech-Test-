e2a2628848749e06ccafb7a67ececa1e51690e7e
AliciaMerlino <acmerlino1@gmail.com>
2020-02-27 11:41:36 +0000

    changes current turn after a player makes move

--- lib/game.rb
+++ lib/game.rb
@@ -5,4 +5,12 @@ class Game
   def initialize
     @current_turn = 'Player 1'
   end
+
+  def change_turn
+    if @current_turn == 'Player 1'
+      @current_turn = 'Player 2'
+    else
+      @current_turn = 'Player 1'
+    end
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,22 @@ require 'game'
 describe Game do
   
   describe '#current_turn' do
-    it 'returns player 1 and current turn' do
+    it 'returns player 1 for current turn at beginning of game' do
       expect(subject.current_turn).to eq('Player 1')
     end
   end
+
+  describe '#change_turn' do
+    it 'returns player 2 for current turn' do
+      subject.change_turn
+      expect(subject.current_turn).to eq('Player 2')
+    end
+
+    it 'returns player 1 for current turn after player 2s turn' do
+      subject.change_turn
+      subject.change_turn
+      expect(subject.current_turn).to eq('Player 1')
+    end
+  end
+  
 end
